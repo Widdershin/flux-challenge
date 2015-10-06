075e99f470ba4ce8e75d9f126c4d0994436df089
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-07 09:45:59 +1300

    Add HTML for sith list

--- submissions/Widdershin/src/sith-dashboard.js
+++ submissions/Widdershin/src/sith-dashboard.js
@@ -6,6 +6,43 @@ Rx.DOM = DOM;
 
 const PLANET_SOCKET_ADDRESS = 'ws://localhost:4000';
 
+function planetMonitor (planet) {
+  return h('h1.css-planet-monitor', `Obi Wan is currently on ${planet.name}`);
+}
+
+function scrollButtons () {
+  return (
+    h('.css-scroll-buttons', [
+      h('.css-button-up'),
+      h('.css-button-down')
+    ])
+  );
+}
+
+function sithSlot (sithLord) {
+  return (
+    h('li.css-slot', [
+      h('h3', sithLord.name),
+      h('h6', `Homeworld: ${sithLord.homeworld.name}`)
+    ])
+  );
+}
+
+function sithSlots (sithLords) {
+  return (
+    h('ul.css-slots', sithLords.map(sithSlot))
+  );
+}
+
+function sithList () {
+  return (
+    h('section.css-scrollable-list', [
+      sithSlots([]),
+      scrollButtons()
+    ])
+  );
+}
+
 export default function sithDashboard ({DOM}) {
   const planet$ = Rx.DOM.fromWebSocket(PLANET_SOCKET_ADDRESS)
     .map(message => JSON.parse(message.data))
@@ -15,7 +52,9 @@ export default function sithDashboard ({DOM}) {
     DOM: planet$.map(planet => {
       return (
         h('.css-root', [
-          h('h1.css-planet-monitor', `Obi Wan is currently on ${planet.name}`)
+          planetMonitor(planet),
+
+          sithList()
         ])
       );
     })
