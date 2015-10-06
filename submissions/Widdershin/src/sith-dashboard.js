84cad1611b9c24c64630bbd7181bb0be07599b57
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-07 09:33:31 +1300

    Set up websockets with Rx DOM

--- submissions/Widdershin/src/sith-dashboard.js
+++ submissions/Widdershin/src/sith-dashboard.js
@@ -1,10 +1,21 @@
 import {Rx} from '@cycle/core';
+import {DOM} from 'rx-dom';
 import {h} from '@cycle/dom';
 
+Rx.DOM = DOM;
+
+const PLANET_SOCKET_ADDRESS = 'ws://localhost:4000';
+
 export default function sithDashboard ({DOM}) {
+  const planet$ = Rx.DOM.fromWebSocket(PLANET_SOCKET_ADDRESS)
+    .map(message => JSON.parse(message.data))
+    .startWith({id: 0, name: 'Earth'});
+
   return {
-    DOM: Rx.Observable.just([]).map(sithLords => (
-      h('.sithLords', sithLords.join())
-    ))
+    DOM: planet$.map(planet => {
+      return (
+        h('.sithLords', planet.name)
+      )
+    })
   };
 }
